906463214da2cbec9d8a9ea20bee1d5d454ca179
umer ali <sheikhumer973@gmail.com>
2021-06-06 12:28:15 -0700

    file upload api implementations

---
+++ RadixBackOfficeAPI/Controllers/FileUploadController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+using System.Web.UI.WebControls;
+
+namespace RadixBackOfficeAPI.Controllers
+{
+    public class FileUploadController : ApiController
+    {
+        [HttpPost]
+        [Route("api/UploadImage")]
+        public IHttpActionResult UploadImage()
+        {
+            string imageName = null;
+            var httpRequest = HttpContext.Current.Request;
+            //Upload Image
+            var postedFile = httpRequest.Files["image"];
+            //Create custom filename
+            if (postedFile != null)
+            {
+                imageName = new String(Path.GetFileNameWithoutExtension(postedFile.FileName).Take(10).ToArray()).Replace(" ", "-");
+                imageName = imageName + DateTime.Now.ToString("yymmssfff") + Path.GetExtension(postedFile.FileName);
+                var filePath = HttpContext.Current.Server.MapPath("~/Images/" + imageName);
+                postedFile.SaveAs(filePath);
+            }
+            return Ok();
+        }
+
+        [HttpPost]
+        [Route("api/UploadFile")]
+        public IHttpActionResult UploadFile()
+        {
+            string filename = null;
+            var httpRequest = HttpContext.Current.Request;
+            //Upload Image
+            var postedFile = httpRequest.Files["file"];
+            //Create custom filename
+            if (postedFile != null)
+            {
+                filename = new String(Path.GetFileNameWithoutExtension(postedFile.FileName).Take(10).ToArray()).Replace(" ", "-");
+                filename = filename + DateTime.Now.ToString("yymmssfff") + Path.GetExtension(postedFile.FileName);
+                var filePath = HttpContext.Current.Server.MapPath("~/Downloads/" + filename);
+                postedFile.SaveAs(filePath);
+            }
+            return Ok();
+        }
+
+
+
+    }
+}
